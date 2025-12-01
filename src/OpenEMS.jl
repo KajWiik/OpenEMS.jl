fd96a27349b5ae64274d046bf1c573370a7a62fe
Kaj Wiik <kaj.wiik@utu.fi>
2025-12-01 23:03:21 +0200

    Move imports to __init__ to fix segfault

--- src/OpenEMS.jl
+++ src/OpenEMS.jl
@@ -1,15 +1,19 @@
 module OpenEMS
 
-if !haskey(ENV, "JULIA_PYTHONCALL_EXE")
-    ENV["JULIA_PYTHONCALL_EXE"] = joinpath(homedir(), ".local", "openEMS", "venv", "bin", "python")
-end
-
-ENV["JULIA_CONDAPKG_BACKEND"] = "Null"
-
 using PythonCall
 
-CSXCAD = pyimport("CSXCAD")
-openEMS = pyimport("openEMS")
+const CSXCAD = PythonCall.pynew()
+const openEMS = PythonCall.pynew()
+
+function __init__()
+    if !haskey(ENV, "JULIA_PYTHONCALL_EXE")
+        ENV["JULIA_PYTHONCALL_EXE"] = joinpath(homedir(), ".local", "openEMS", "venv", "bin", "python")
+    end
+    ENV["JULIA_CONDAPKG_BACKEND"] = "Null"
+
+    PythonCall.pycopy!(CSXCAD, pyimport("CSXCAD"))
+    PythonCall.pycopy!(openEMS, pyimport("openEMS"))
+end
 
 export CSXCAD, openEMS
 
